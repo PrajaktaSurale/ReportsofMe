6e83535202ab5d1654e27af9df94d3fdb720b7ce
PrajaktaSurale <prajudpatil@gmail.com>
2025-05-06 13:11:23 +0530

    Update auth.go

--- middleware/auth.go
+++ middleware/auth.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 )
+
 func AuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		session := sessions.Default(c)
